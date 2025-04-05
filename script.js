ff3996d0c1669217b796032e3f335359b1751982
Richi78 <ricardo.rojas.carvajal@gmail.com>
2025-04-05 10:09:37 -0400

    test

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el botón con id "0"
-    const celda0 = document.getElementById('0');
-    
-    // Agregar un event listener para el evento click
-    if (celda0) {
-        celda0.addEventListener('click', function() {
-            console.log('Has presionado la celda con id 0');
-        });
-    } else {
-        console.error('No se encontró el elemento con id 0');
-    }
-});
+
+function funciona(){
+    console.log("object")
+}
